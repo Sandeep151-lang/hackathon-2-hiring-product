07fca97a6e829a17c96612c371cf585ed1925b52
Sandeep <sandeepnandanwar92@gmail.com>
2022-03-11 12:23:52 +0530

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,9 +3,16 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='footer'>
-            <p className='text-white text-center'>Footer</p>
-            <p className='text-white text-center'>Contact Us</p>
+        <div className='container-fluid d-flex justify-content-evenly text-white' style={{ 'backgroundColor': 'black' }}>
+            <div>
+                <p className='mt-5'>Contact No : <span>8738854495</span></p>
+                <p>Email: sandeepnandawar92@gmail.com</p>
+            </div>
+            <div>
+                <p className='mt-3 mx-5'>Address : Nagpur</p>
+                <p className='mx-5'>Email: hiringKart@gmail.com</p>
+                <p className='mx-5'>&#169; 2022  hiringKart.com</p>
+            </div>
         </div>
     )
 }
