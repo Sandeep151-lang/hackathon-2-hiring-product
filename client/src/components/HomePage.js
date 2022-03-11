f986e4a4ae69c6173ddc4d4d636b1b8433aca4ee
Sandeep <sandeepnandanwar92@gmail.com>
2022-03-11 12:20:16 +0530

    Update HomePage.js

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { Card, Row, Col } from 'reactstrap';
-//import { useHistory } from "react-router-dom";
-//import { MyContext } from '../App';
-//import Pagination from "react-js-pagination";
+import { Card, Row, Col, Container } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
-// import axios from 'axios';
+
 import LoadingSpinner from './LoadingSpinner';
 import Navbar from './Navbar';
+import Footer from './Footer';
+import FooterComponent from './FooterComponent';
 
 const HomePage = () => {
     const history = useHistory();
@@ -16,23 +15,31 @@ const HomePage = () => {
     const [loading, setloading] = useState(true);
     const [dat, setdat] = useState([])
     const [search, setsearch] = useState(name)
-     const [category, setcategory] = useState([]);
+    const [category, setcategory] = useState([]);
+
+
 
     const data = async () => {
-        const res = await axios.get(`/getProducts/?keyword=${search}`)
-        setdat(res.data.products)
-        setloading(false);
+        try {
+            const res = await axios.get(`/getProducts/?keyword=${search}`)
+            setdat(res.data.products)
+            setloading(false);
+        } catch (err) {
+            console.log(err.response.data)
+        }
+
         //console.log(res)
     }
 
     const net = async (e) => {
-        console.log(e)
+
         history.push(`/details/${e._id}`)
     }
     //const context = useContext(MyContext);
- const categories = async () => {
+
+    const categories = async () => {
         try {
-            const res = await axios.get(`http://localhost:5000/getProduct`)
+            const res = await axios.get(`/getProduct`)
             setcategory(res.data.message)
             setloading(false);
 
@@ -43,7 +50,7 @@ const HomePage = () => {
         //console.log(res)
     }
 
-  
+
     useEffect(() => {
         data()
         categories()
@@ -51,41 +58,49 @@ const HomePage = () => {
     }, [search])
 
 
-    return (
-        <div className='' style={{ 'position': 'relative' }}>
-            <Navbar />
-            <Row>
-                <Col sm='2' className='m-4 w3-card-4' style={{ 'height': '100%' }}>
-                    <input type="text" className="form-control mt-3" placeholder='search' id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e) => { setsearch(e.target.value) }} onKeyPress={(e) => e.key === 'Enter' ? data() : null} />
-                    <ul>
-                        {
-                            category.map((item, key) => {
-                                return <>
-                                    {/* <button onClick={(e) => setsearch(item.name)} value={item.name} type="text" className="form-control mt-3" >{item.name}</button> */}
-                                    <li onClick={(e) => setsearch(item.name)} value={item.name} type="text" className="list-category mt-3" >{item.name}</li>
-                                </>
-                            })
-                        }
-                    </ul>
-                </Col>
-                {loading ? <LoadingSpinner /> : <>
-                    {dat.map((product, key) => {
+    return <>
+
+        <Navbar />
+
+        <div className='w3-card-4 container-fluid mt-1'>
+            {
+                category.map((item, key) => {
+                    return <>
+                        {/* <button onClick={(e) => setsearch(item.name)} value={item.name} type="text" className="form-control mt-3" >{item.name}</button> */}
+                        <span key={key} onClick={(e) => setsearch(item.name)} value={item.name} type="text" className="list-category mt-3 mx-4 p-2" >{item.name}</span>
+                    </>
+                })
+            }
+        </div>
+        <Container>
+            <input type="text" className=" mt-3" placeholder='search' id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e) => { setsearch(e.target.value) }} onKeyPress={(e) => e.key === 'Enter' ? data() : null} />
+        </Container>
+        <div className='container mt-3 mb-5 card-category'>
+            {loading ? <LoadingSpinner /> : <Row>
+                {
+                    dat.map((product, key) => {
                         const { image, product_name, price } = product
+                        //destructuring the products 
                         return (
-                            <Col sm="2" key={key} className="py-3 container-fluid" xs={3}>
-                                <Card body onClick={(e) => { net(product) }} className='category'>
-                                    <img src={image} className="card-img-top" alt="..." style={{ 'objectFit': 'scale-down' }} />
-                                    <h6 className="text-center bold">{product_name}</h6>
-                                    <h4 className="text-center bold">&#8377;{price}/<small>hr</small></h4>
+                            <Col sm="3" key={key} className="py-3 category">
+                                <Card body onClick={(e) => { net(product) }} style={{ 'height': '15rem' }} >
+                                    <img src={image} className="card-img-top" alt="..." height='100px' width='50px' />
+                                    <div className="card-body">
+                                        <p className="card-text" style={{ 'overflow': 'hidden', 'whiteSpace': 'nowrap', 'textOverflow': 'ellipsis' }}>{product_name}</p>
+                                        <small className="card-text" style={{ 'fontWeight': 'bold' }}>&#8377;{price}/hr</small>
+                                    </div>
                                 </Card>
                             </Col>
                         )
-                    })}
-                </>
+                    })
                 }
             </Row>
-        </div>
-    )
+            }
+        </div >
+        <FooterComponent />
+        <Footer />
+
+    </>
 }
 
 export default HomePage
