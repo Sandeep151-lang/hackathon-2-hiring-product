cd914b3747f2dc405b91ab7460526c15f4d1fe2b
Sandeep Nandanwar <sandeepnandanwar92@gmail.com>
2023-04-23 13:17:22 +0530

    script fix

--- app.js
+++ app.js
@@ -1,87 +1,42 @@
 
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var app = express();
+//var path = require('path');
+//var logger = require('morgan');
 var createProduct = require('./routes/createProduct')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var payment = require('./routes/payments')
 const cors = require("cors");
-var app = express();
-
-// view engine setup
-// if (process.env.NODE_ENV=="production"){
-//   const path = require('path')
-
-//   app.get('/',(req,res)=>{
-//     app.use(express.static(path.resolve(__dirname,'client','build')))
-//     res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//   })
-// }
-  
-  
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-// const corsOptions = {
-//   origin: true, //included origin as true
-//   credentials: true, //included credentials as true
-// };
-// app.use(cors(corsOptions));
-app.use(cors({origin:"*"}))
-
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header('Access-Control-Allow-Credentials', false);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  next()
-})
-
-app.get('/test/list',(req,res)=>{
-  res.send('hello')
-})
-app.use('/', indexRouter);
-app.use('/', usersRouter);
-app.use('/', payment)
-app.use('/', createProduct);
-
-
-app.listen( 5000, (err,d)=>{
-  if(err) console.log(err)
-  console.log(`server is running in port ${500}`)
-})
-
-
-// if (process.env.Mode_Env = "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", function (request, response) {
-//     response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-//   });
-
-// }
 
 
 
-// catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
+require('./routes/dbConn.js/conn')
 
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+    app.use(cors({origin:"*"}))
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
-module.exports = app;
+    app.use(function (req, res, next) {
+      res.header("Access-Control-Allow-Headers", "*");
+      res.header('Access-Control-Allow-Credentials', false);
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      next()
+    })
+    
+    app.get('/test/list',(req,res)=>{
+      res.send('hello')
+    })
+    app.use('/', indexRouter);
+    app.use('/', usersRouter);
+    app.use('/', payment)
+    app.use('/', createProduct);
+    
+    
+    app.listen( 5000, (err,d)=>{
+      if(err) console.log(err)
+      console.log(`server is running in port ${500}`)
+    })
+
+
+
+  module.exports = app;
